cc94f2df904a3ab2ff1a0199dbf255f3c1bbceb7
Mohammed Subhani <mohammedsubhani201@gmail.com>
2025-12-03 20:52:54 +0530

    Second commit

--- variables.c
+++ variables.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    
+    int a,b;
+    a=29;
+    b=34;
+    a-=b;
+    printf("%d\n",a);
+    printf("Enter the value of  a\n");  
+    scanf("%d",&a);    
+    printf("Enter the value of b\n");     
+    scanf("%d",&b);
+    a+=b;
+    printf("%d",a);
+
     return 0;
 }
