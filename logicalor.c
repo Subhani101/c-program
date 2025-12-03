2396c9bd63aa40dc64de6bea27c27b85d4c19a58
Mohammed Subhani <mohammedsubhani201@gmail.com>
2025-12-03 22:54:02 +0530

    logicalor testing

---
+++ logicalor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    // int issunday,issnowing;
+    // printf("Enter the value of issunda : \n");
+    // scanf("%d",issunday);
+    // printf("Enter the value of is issnowing \n :");
+    // scanf("%d",issnowing);
+    // printf("%d", issnowing || issunday);
+    // printf("%d",issnowing&&issunday);
+int Sunday,Snowing;
+Sunday=0;
+Snowing=0;
+printf("%d",Sunday||Snowing);
+    return 0;
+}
