83e0458fdcc8191db31d6379248b7ff3c27453c6
Mohammed Subhani <mohammedsubhani201@gmail.com>
2025-12-03 20:59:10 +0530

    Second commit

--- variables.c
+++ variables.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
+<<<<<<< Updated upstream
     
+=======
+    int a,b;
+    a=29;
+    b=34;
+    a-=b;
+    printf("%d\n",a);
+    printf("Enter the value of  a\n");  
+    scanf("%d",&a);    
+    printf("Enter the value of b\n");     
+    scanf("%d",&b);
+    a+=b;
+    printf("%d",a);
+>>>>>>> Stashed changes
     return 0;
 }
